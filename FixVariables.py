534ba8dba39d5a6b2a4fc3764607ecdc0621b622
T3ddy23 <evansc1829@students.clarkstate.edu>
2025-01-31 05:23:45 -0500

    Update FixVariables.py
    your video say to not change cap letters. i keep getting error for THEFT

--- FixVariables.py
+++ FixVariables.py
@@ -4,11 +4,11 @@
 ### The output should look like the follow line without the ###
 ### Hello, world. There were 10 cakes, but Lex Luthor stole 2. Now there's 8. And that's sad.
 
-the Hello = "Hello"
-the.World = "world"
+hello = "Hello"
+world = "world"
 cakes = 10
 theft = 2
 
 remaining = cakes - THEFT
 
-print(f"{thehello}, {theWorld}. There were {10cakes} cakes, but Lex Luthor stole {theft}. Now there's {remaining}. And that's sad.")
+print("{hello}, {world}. There were {10,cakes} cakes, but Lex Luthor stole {theft}. Now there's {remaining}. And that's sad.")
