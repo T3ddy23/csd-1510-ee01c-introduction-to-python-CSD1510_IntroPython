efee79c086fa61becbf2b5c5831b71b1a837663d
T3ddy23 <evansc1829@students.clarkstate.edu>
2025-06-06 16:50:13 -0400

    Update AdditionDemo.py

---
+++ AdditionDemo.py
@@ -0,0 +1,9 @@
+print("Please enter three numbers to add together:")
+
+number1 = int(input("Please enter number 1: "))
+number2 = int(input("Please enter number 2: "))
+number3 = int(input("Please enter number 3: "))
+
+sum = number1 + number2 + number3
+
+print(f"The sum of {number1} and {number2} and {number3} is {sum}")
