d923bee91cead97e594d688f8b4796a6671f70e7
T3ddy23 <evansc1829@students.clarkstate.edu>
2025-06-06 17:04:14 -0400

    Update AverageAssignment.py

---
+++ AverageAssignment.py
@@ -0,0 +1,9 @@
+print("Please enter three integers to average")
+
+number1 = int(input("Please enter number 1: "))
+number2 = int(input("Please enter number 2: "))
+number3 = int(input("Please enter number 3: "))
+
+average = number1 + number2 + number3
+
+print(f"The avearge of {number1} and {number2} and {number3} is {average}")
