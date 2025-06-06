85aedf04be23cb222dd25ae204364a8ffcb3363d
T3ddy23 <evansc1829@students.clarkstate.edu>
2025-06-06 17:14:54 -0400

    Update FixVariables.py

--- FixVariables.py
+++ FixVariables.py
@@ -9,6 +9,6 @@ world = "world"
 cakes = 10
 theft = 2
 
-remaining = cakes - THEFT
+remaining = cakes - theft
 
-print("{hello}, {world}. There were {10,cakes} cakes, but Lex Luthor stole {theft}. Now there's {remaining}. And that's sad.")
+print(f"{hello}, {world}. There were {cakes} cakes, but Lex Luthor stole {theft}. Now there's {remaining}. And that's sad.")
